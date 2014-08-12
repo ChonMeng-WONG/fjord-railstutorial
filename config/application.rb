f9c6716275d2d8730fea607cdbbcb1c049bbc213
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-08-12 16:19:16 +0900

    chapter5-origin

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
+require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -11,7 +13,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Intern
+module SampleApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
