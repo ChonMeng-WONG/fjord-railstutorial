2ae677c4e33e0c0836016e3221aeb19a2534216d
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-08-22 17:17:48 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,49 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
-	def show
-		@user = User.find(params[:id])
-	end
-	
+  def index
+  	@users = User.paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
+    @user = User.new(user_params)
+    if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-  	else
-  		render 'new'
-  	end
+    else
+      render 'new'
+    end
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
-  def udpate
-    @user = User.find(params[:id])
+  def update
+   # @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
     else
       render 'edit'
     end
+  end
+
+  def destroy
+  	User.find(params[:id]).destroy
+  	flash[:success] = "User destroyed"
+  	redirect_to users_url
   end
 
   private
@@ -42,6 +54,19 @@ class UsersController < ApplicationController
 
     # Before actions
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    	unless signed_in?
+    		store_location
+      	redirect_to signin_url, notice: "Please sign in." 
+      end
     end
+
+    def correct_user
+    	@user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+    	redirect_to(root_path) unless current_user.admin?
+    end
+
 end
